e85dd24646cb3ac600ff324bef4369acd2041527
Daniel Gyrén <Daniel.Gyren@outlook.com>
2022-03-22 03:38:45 +0100

    Add project files.

---
+++ Labb 3 – Trådar och asynkront/Car.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Labb_3___Trådar_och_asynkront
+{
+    public class Car
+    {
+        private string _carName;
+        private string _carColour;
+        private int _carSpeed;
+        private int _carDistanceTraveled;
+        private int _trackLength;
+
+        public Car(string carName, string carColour, int carSpeed, int trackLength)
+        {
+            this._carName = carName;
+            this._carColour = carColour;
+            this._carSpeed = carSpeed;
+            this._carDistanceTraveled = 0;
+            this._trackLength = trackLength;
+        }
+
+        public void PrintAllTheCars()
+        {
+            Console.WriteLine($"Name of the car : {_carName}.");
+            Console.WriteLine($"Colour of the car : {_carColour}.");
+            Console.WriteLine($"Speed of the car : {_carSpeed} km/h.");
+            Console.WriteLine($"Distance left for the car to drive : {_trackLength - _carDistanceTraveled} kilometers.");
+        }
+
+        public static List<string> _leaderboard = new List<string>();
+
+        private bool _race = true;
+        public void RaceInProgress()
+        {
+            while (_race == true)
+            {
+                for (_carDistanceTraveled = 0; _carDistanceTraveled <= _trackLength; _carDistanceTraveled++)
+                {
+                    Thread.Sleep(7000);
+
+                    Random randomEvent = new Random();
+                    int re = randomEvent.Next(0, 51);
+
+                    if (re == 1)
+                    {
+                        if (Thread.CurrentThread.Name == "Demolisher")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine($"{Thread.CurrentThread.Name} has run out of gas! It needs to be refuelled.");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Thread.Sleep(30000);
+                            Console.WriteLine();
+                        }
+                        if (Thread.CurrentThread.Name == "Prowler")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine($"{Thread.CurrentThread.Name} has run out of gas! It needs to be refuelled.");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Thread.Sleep(30000);
+                            Console.WriteLine();
+                        }
+                        if (Thread.CurrentThread.Name == "Centurion")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"{Thread.CurrentThread.Name} has run out of gas! It needs to be refuelled.");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Thread.Sleep(30000);
+                            Console.WriteLine();
+                        }
+                    }
+
+                    else if (re == 2 || re == 3)
+                    {
+                        if (Thread.CurrentThread.Name == "Demolisher")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine($"{Thread.CurrentThread.Name} has blown a tire! It needs to be replaced.");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Thread.Sleep(20000);
+                            Console.WriteLine();
+                        }
+                        if (Thread.CurrentThread.Name == "Prowler")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine($"{Thread.CurrentThread.Name} has blown a tire! It needs to be replaced.");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Thread.Sleep(20000);
+                            Console.WriteLine();
+                        }
+                        if (Thread.CurrentThread.Name == "Centurion")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"{Thread.CurrentThread.Name} has blown a tire! It needs to be replaced.");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Thread.Sleep(20000);
+                            Console.WriteLine();
+                        }
+                    }
+
+                    else if (re > 3 && re < 9)
+                    {
+                        if (Thread.CurrentThread.Name == "Demolisher")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine($"A bird just smashed into {Thread.CurrentThread.Name}’s front window! It will take some time to clean it.");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Thread.Sleep(10000);
+                            Console.WriteLine();
+                        }
+                        if (Thread.CurrentThread.Name == "Prowler")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine($"A bird just smashed into {Thread.CurrentThread.Name}’s front window! It will take some time to clean it.");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Thread.Sleep(10000);
+                            Console.WriteLine();
+                        }
+                        if (Thread.CurrentThread.Name == "Centurion")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"A bird just smashed into {Thread.CurrentThread.Name}’s front window! It will take some time to clean it.");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Thread.Sleep(10000);
+                            Console.WriteLine();
+                        }
+                    }
+
+                    else if (re > 8 && re < 19)
+                    {
+                        if (Thread.CurrentThread.Name == "Demolisher")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine($"{Thread.CurrentThread.Name} has just experienced an issue with its engine!");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            _carSpeed = _carSpeed - 1;
+                            Console.WriteLine();
+                        }
+                        if (Thread.CurrentThread.Name == "Prowler")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine($"{Thread.CurrentThread.Name} has just experienced an issue with its engine!");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            _carSpeed = _carSpeed - 1;
+                            Console.WriteLine();
+                        }
+                        if (Thread.CurrentThread.Name == "Centurion")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"{Thread.CurrentThread.Name} has just experienced an issue with its engine!");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            _carSpeed = _carSpeed - 1;
+                            Console.WriteLine();
+                        }
+                    }
+
+                    if (_carDistanceTraveled == _trackLength)
+                    {
+                        if (Thread.CurrentThread.Name == "Demolisher")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                            Console.WriteLine($"{Thread.CurrentThread.Name} has just crossed the finish line!");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            _leaderboard.Add(_carName);
+                            Console.WriteLine();
+                            _race = false;
+                        }
+                        if (Thread.CurrentThread.Name == "Prowler")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Blue;
+                            Console.WriteLine($"{Thread.CurrentThread.Name} has just crossed the finish line!");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            _leaderboard.Add(_carName);
+                            Console.WriteLine();
+                            _race = false;
+                        }
+                        if (Thread.CurrentThread.Name == "Centurion")
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"{Thread.CurrentThread.Name} has just crossed the finish line!");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            _leaderboard.Add(_carName);
+                            Console.WriteLine();
+                            _race = false;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Labb 3 – Trådar och asynkront/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Threading;
+
+namespace Labb_3___Trådar_och_asynkront
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car1 = new Car("Demolisher", "Yellow", 100, 10);
+            Car car2 = new Car("Prowler", "Blue", 100, 10);
+            Car car3 = new Car("Centurion", "Red", 100, 10);
+
+            Thread tCar1 = new Thread(car1.RaceInProgress);
+            Thread tCar2 = new Thread(car2.RaceInProgress);
+            Thread tCar3 = new Thread(car3.RaceInProgress);
+
+            tCar1.Name = "Demolisher";
+            tCar2.Name = "Prowler";
+            tCar3.Name = "Centurion";
+
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("Input status if you want to see the cars current status.");
+            Console.WriteLine("----------------------------------------------------------------");
+
+            Console.WriteLine();
+            Console.WriteLine("Rev up your engines! The race will commence in 3..."); Thread.Sleep(1500);
+            Console.WriteLine("2..."); Thread.Sleep(1500);
+            Console.WriteLine("1..."); Thread.Sleep(1500);
+            Console.WriteLine("Go! Burn rubber!");
+            Console.WriteLine();
+
+            tCar1.Start();
+            tCar2.Start();
+            tCar3.Start();
+
+            while (tCar1.IsAlive || tCar2.IsAlive || tCar3.IsAlive)
+            {
+                string printStatus = Console.ReadLine();
+                if (printStatus == "status")
+                {
+                    car1.PrintAllTheCars();
+                    Console.WriteLine();
+                    car2.PrintAllTheCars();
+                    Console.WriteLine();
+                    car3.PrintAllTheCars();
+                }
+            }
+
+            Console.WriteLine("The results are in!");
+            Console.WriteLine($"In third place we have {Car._leaderboard[2]}"); Thread.Sleep(2000);
+            Console.WriteLine($"In second place we find {Car._leaderboard[1]}"); Thread.Sleep(2000);
+            Console.WriteLine($"That means {Car._leaderboard[0]} has taken the first spot on the pedestal! Congratulations!!!");
+
+            Console.ReadKey();
+        }
+    }
+}
